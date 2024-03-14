d17b213a7feefe0eea3fc24d79a7521ba506a257
Joao Ávila <jolucas245@gmail.com>
2024-03-14 20:43:11 -0300

    :tada: feat: Creating repository! Yeah!

---
+++ app/src/main/java/com/fiap/zerodengue/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.fiap.zerodengue
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fiap.zerodengue.ui.theme.ZeroDengueTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ZeroDengueTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
