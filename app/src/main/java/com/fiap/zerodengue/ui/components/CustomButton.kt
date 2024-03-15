c6be005c785bc2f84db356eca0b7255a80c731da
Joao Ávila <jolucas245@gmail.com>
2024-03-15 10:31:33 -0300

    :sparkles: feat: Initial View created and componentized buttons

---
+++ app/src/main/java/com/fiap/zerodengue/ui/components/CustomButton.kt
@@ -0,0 +1,51 @@
+package com.fiap.zerodengue.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Text
+
+@Composable
+fun GradientButton(
+    gradient: Brush,
+    elevation: ButtonElevation,
+    modifier: Modifier,
+    onClick: () -> Unit,
+    content: @Composable BoxScope.() -> Unit
+) {
+    Button(
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent
+        ),
+        contentPadding = PaddingValues(),
+        modifier = modifier,
+        elevation = elevation,
+        onClick = { onClick() })
+    {
+        Box(
+            modifier = Modifier
+                .background(gradient)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            content()
+        }
+    }
+}
---
+++ app/src/main/java/com/fiap/zerodengue/ui/components/CustomTextField.kt
@@ -0,0 +1,8 @@
+package com.fiap.zerodengue.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomTextField() {
+
+}
